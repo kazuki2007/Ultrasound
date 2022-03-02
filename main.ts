5c9ed1035a700cf1023475321203ead8f2583c9f
kazuki2007 <95865549+kazuki2007@users.noreply.github.com>
2022-03-02 12:20:57 +0900

    最終決定版

--- main.ts
+++ main.ts
@@ -1,42 +1,62 @@
+let 状態 = 0
+input.onButtonPressed(Button.A, function () {
+    状態 = 0
+})
+input.onButtonPressed(Button.B, function () {
+    状態 = 1
+})
 basic.forever(function () {
-    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 20) {
-        if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 5) {
-            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
-            basic.showLeds(`
-                # . . . #
-                . # . # .
-                . . # . .
-                . . . . .
-                . . . . .
-                `)
-            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
-            basic.pause(100)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+    if (状態 == 0) {
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 20) {
+            if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 5) {
+                maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+                maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+                basic.showLeds(`
+                    # . . . #
+                    . # . # .
+                    . . # . .
+                    . . . . .
+                    . . . . .
+                    `)
+                maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+                basic.pause(100)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+            } else {
+                basic.showLeds(`
+                    . . # . .
+                    . . # . .
+                    . . # . .
+                    . . . . .
+                    . . # . .
+                    `)
+                maqueen.motorStop(maqueen.Motors.All)
+                maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+                maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+                basic.pause(100)
+                maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+            }
         } else {
             basic.showLeds(`
-                . . # . .
-                . . # . .
-                . . # . .
+                # # # # #
                 . . . . .
-                . . # . .
+                . . . . .
+                . . . . .
+                . . . . .
                 `)
-            maqueen.motorStop(maqueen.Motors.All)
-            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
             maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-            basic.pause(100)
-            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
         }
     } else {
         basic.showLeds(`
-            # # # # #
+            # # . # #
             . . . . .
+            # . # . #
             . . . . .
-            . . . . .
-            . . . . .
+            # # . # #
             `)
-        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
-        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
-        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+        maqueen.motorStop(maqueen.Motors.All)
     }
 })
