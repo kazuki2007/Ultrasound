afe9263f551c147cbba045edba61ca0be3e6ab20
kazuki2007 <95865549+kazuki2007@users.noreply.github.com>
2022-03-02 12:15:15 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,42 @@
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 20) {
+        if (maqueen.Ultrasonic(PingUnit.Centimeters) <= 5) {
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOff)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            basic.showLeds(`
+                # . . . #
+                . # . # .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+            basic.pause(100)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 50)
+        } else {
+            basic.showLeds(`
+                . . # . .
+                . . # . .
+                . . # . .
+                . . . . .
+                . . # . .
+                `)
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOff)
+            maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+            basic.pause(100)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 100)
+        }
+    } else {
+        basic.showLeds(`
+            # # # # #
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 150)
+        maqueen.writeLED(maqueen.LED.LEDLeft, maqueen.LEDswitch.turnOn)
+        maqueen.writeLED(maqueen.LED.LEDRight, maqueen.LEDswitch.turnOn)
+    }
+})
